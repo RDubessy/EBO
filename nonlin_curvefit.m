f22847f84c86bb84eec4473c17c32bf2ec009869
BEC009-T3600 <BEC@BEC009-T3600>
2015-01-20 17:13:49 +0100

    Curve fitting messages disabled for matlab.

--- nonlin_curvefit.m
+++ nonlin_curvefit.m
@@ -1,4 +1,5 @@
 function p = nonlin_curvefit (f, p0, x, y)
 % NONLIN_CURVEFIT Redefinition for compatibility with Octave.
-p = lsqcurvefit (f,p0,x,y);
+options = optimset('Display','off');
+p = lsqcurvefit (f,p0,x,y,[],[],options);
 end
