6d6c1f3e595215a502e18ab6ace4da5f64bc89b0
Romain Dubessy <romain.dubessy@github.com>
2015-06-09 18:36:45 +0200

    Code cleanup

--- computeMatrix.m
+++
@@ -1,9 +0,0 @@
-function [M] = computeMatrix (A,B,Bc,C,Omega,delta)
-% COMPUTEMATRIX Compute the Liouville operator governing the density matrix
-% evolution.
-    M = A+C*delta;
-    Omegac = conj(Omega);
-    for i=1:3
-        M(:,:) = M(:,:) + Omega(i) * B(:,:,i) + Omegac(i) * Bc(:,:,i);
-    end
-end
--- nonlin_curvefit.m
+++
@@ -1,5 +0,0 @@
-function p = nonlin_curvefit (f, p0, x, y)
-% NONLIN_CURVEFIT Redefinition for compatibility with Octave.
-options = optimset('Display','off');
-p = lsqcurvefit (f,p0,x,y,[],[],options);
-end
